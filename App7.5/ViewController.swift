b9b98a65a250a7c3e741c3ec4bc4fb43b674b2fc
Ekolu <Ekolu@kiprass-mac.local>
2016-03-21 12:57:15 +0100

    Initial Commit

---
+++ App7.5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App7.5
+//
+//  Created by Kipras on 3/21/16.
+//  Copyright © 2016 Kipras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
